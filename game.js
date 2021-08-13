b4c75a87e6d246bbcae17efc74e03943333d0bee
=Parul Mahajan <=parulmahajan1212001@gmail.com>
2021-08-13 08:44:18 +0530

    added flyer

--- game.js
+++ game.js
@@ -1,68 +1,96 @@
-buttoncolors=["red","blue","green","yellow"];
-gamePattern=[];
-var level=0;
-starts=false;
-$(document).keypress(function(){
-    if(!starts){
-        $("#level-title").text("Level " + level);
+buttoncolors = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+var level = 0;
+starts = false;
+$(".start").click(function () {
+  if (!starts) {
+    $("#level-title").text("Level " + level);
+    $(".start").text("Restart");
+    nextSequence();
+    starts = true;
+  } else {
+    $("#level-title").text("");
+    $(".start").text("Start");
+    startOver();
+  }
+});
+// $(document).keypress(function () {
+//   if (!starts) {
+//     $("#level-title").text("Level " + level);
+//     nextSequence();
+//     starts = true;
+//   }
+// });
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  playsound(userChosenColour);
+  animatePress(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  checkanswer(userClickedPattern.length - 1);
+});
+function checkanswer(cuulevel) {
+  if (gamePattern[cuulevel] === userClickedPattern[cuulevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function () {
         nextSequence();
-        starts = true;
+      }, 1000);
     }
-});
-$(".btn").click(function() {
-        var userChosenColour = $(this).attr("id");
-        playsound(userChosenColour);
-        animatePress(userChosenColour);
-        userClickedPattern.push(userChosenColour);  
-        checkanswer(userClickedPattern.length-1);
-    
-});
-function checkanswer(cuulevel){
-    if(gamePattern[cuulevel] === userClickedPattern[cuulevel]){
-        if(gamePattern.length === userClickedPattern.length){
-            setTimeout(function () {
-                nextSequence();
-            }, 1000);
-        }
-    }
-    else {
-        playsound("wrong");
-        $("body").addClass("game-over");
-        $("#level-title").text("Game Over, Press Any Key to Restart");
-  
-        setTimeout(function () {
-          $("body").removeClass("game-over");
-        }, 200);
-  
-        startOver();
-    }
+  } else {
+    playsound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
 }
 
-function nextSequence(){
-    userClickedPattern=[];
-    randomnumber = Math.floor(Math.random()*4)
-    randomChosenColour = buttoncolors[Math.floor(Math.random() * buttoncolors.length)];
-    gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    playsound(randomChosenColour); 
-    level++;
-    $("#level-title").text("Level " + level);
+function nextSequence() {
+  userClickedPattern = [];
+  randomnumber = Math.floor(Math.random() * 4);
+  randomChosenColour =
+    buttoncolors[Math.floor(Math.random() * buttoncolors.length)];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playsound(randomChosenColour);
+  level++;
+  $("#level-title").text("Level " + level);
 }
-function playsound(key){
-    var audio = new Audio("sounds/" + key + ".mp3");
-    audio.play();   
+function playsound(key) {
+  var audio = new Audio("sounds/" + key + ".mp3");
+  audio.play();
 }
-function animatePress(currentColour){
-    $("#" + currentColour).addClass("pressed");
-    setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");}, 100);
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
 
 // nextSequence()
 
-function startOver(){
-    level=0;
-    gamePattern=[];
-    starts=false;
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  starts = false;
 }
+$(".flyer").hide();
 
+$(".flyer-btn").click(function () {
+  $(".flyer").toggle();
+  if ($(".flyer-btn").text() == "How to play") {
+    $(".flyer-btn").text("");
+  }
+  else if($(".flyer-btn").text() == "") {
+    $(".flyer-btn").text("How to play");
+  }
+  
+  //
+});
